7871ad88b65e8e3c80bc11f29eb8fa436dc0071e
Evan Zhou <evanzhoudev@gmail.com>
2026-02-07 12:24:36 -0800

    Format + Lint

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,7 +1,7 @@
 import { RootProvider } from "fumadocs-ui/provider/next"
 import "./global.css"
+import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import { Metadata } from "next"
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 			{ url: "/favicon-dark.ico", media: "(prefers-color-scheme: dark)" },
 		],
 	},
-};
+}
 
 export default function Layout({ children }: LayoutProps<"/">) {
 	return (
